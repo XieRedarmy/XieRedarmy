8d46f91d781d762d98be151a2919c9893733e7b1
XieRedarmy <56301584@qq.com>
2022-10-24 14:12:13 +0800

    Update Profile.java

--- Profile.java
+++ Profile.java
@@ -1,2 +1,24 @@
 public class Profile{
+1 years of experience in software development and management, rich experience in cross-organization coordination and management, good at complex business analysis and design, solving difficult problems in the development process, team building and project risk control. Strong learning ability and professional skills of the followings: 
+	Strong on Java Core, familiar with JAVA WEB related technical specifications, rich experience in Spring, Spring Boot, Spring Cloud framework 
+	Good at the integration of Open-Source technology resources, able to quickly research and integrate into the project according to requirements    
+	Rich experience in the development and design of supply chain finance platform, risk control management, industrial financing, financial transaction, payment and settlement, etc.  
+	Rich experience in application performance monitoring, bottleneck analysis and related product development  
+	Familiar with Devops, using Jenkins, GIT, CI/CD build pipelines delivery
+	Familiar with JIRA, Confluence, Agile/Scrum methodologies
+	Good knowledge on docker 
+	Familiar with MYSQL, Redis, Kafka, Elasticsearch
+	Highly organized with strong capacity to prioritize workload, delegate, deliverable, and steer project completion within established deadlines
+
+co., Ltd.  
+	Guiding and training the engineers, leading the system analysis of complex business and solution of difficult technical problems, be responsible for the coding of core functions, developing and maintaining the common core modules of the system	
+	In charge of the development of O2O system for a large chain group, supply the APIs for front and mobile implementation, including member, product, order, payment, verify and so on, using Springboot, Nacos, Feign, Elasticsearch, Logstash, Redis, MySQL, which is a high-availability and high-performance platform (member, merchant, shop, product, discount, promotion, order, payment, combined payment, offline verification, refund, integral, analysis), including client app, merchant management system, and platform management system
+	Taking charge of the development of supply chain finance platform, supply the core APIs for the business systems, including user, role, permission, login, order, e-contract and so on, using Springboot, Nacos, Feign, Redis, Mysql, E-Contract, that is a complex industry-finance integrated financial platform, including industrial supply chain management system (supplier, customer, product, contract, order, logistics,  stock, financial management, analysis), risk control system, financing client portal, factoring system (product, credit, contract, order, payment, settlement, verification, refund), block chain, and IAM  
+	Taking charge of the Flexible employment system development, utilizing Springboot, Redis, Mysql, E-Contract, E-Payment. It includes Service Provider management, Platform management, Agent management sub systems
+	Responsible for the legacy products maintenance transferred from America and Canada team, development on the new monitoring platform and report progress to stakeholders in various regions
+	Mainly responsible for application performance monitoring, bottleneck analysis and related product development, the main customers are global top 500 customers, such as large banks, insurance, medical group, etc.  
+	Responsible for the design and implementation of the company's new generation of MSSQL monitoring products, mainly involved in solving the high performance and high concurrency issues 
+	As a member of the technical committee, responsible for product sharing, discussion, performance optimization and architecture review across all departments  
+	Built devOps for the entire product lines from zero, using Jenkins, pipeline, xshell, and docker etc
+	Responsible for implementing the monitoring product for the Web Site, Power VM, Amazon EC2, JAVA, and DB, etc.
 }
