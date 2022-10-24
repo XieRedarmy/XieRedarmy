020c36428f399f720cb3b79ecf4b0bb4d4597b7b
XieRedarmy <56301584@qq.com>
2022-10-24 14:49:08 +0800

    Update Util.java

--- Util.java
+++ Util.java
@@ -1,2 +1,80 @@
 public class Util{
+  	Project description：CMS of abc, main business is that enterprise get Letter of Credit from Bank with a pledge of collateral and carry out international trade and settlement. This system was used be composed of struts1, the leader decided to revamp it with struts2 considering the security risk of strut1.
+	Related skills：It's a big difference between struts1 and struts2，such as frame config，parameter transmission and scope, data validation, Exception Handling, file upload, etc.
+	Responsibility description：
+1. Making project plan, track the progress of the project
+2. communicate with customers, timely feedback the stage results 
+3. participate in the design and early development of the implementation scheme
+4. Solving tricky technical problems.
+	Center of App design
+	Project description：This project is a abc project, which is developed using hw Jalor6 framework. The project aims to establish a metadata design center, a designer oriented design platform, connect the design-end to the development-end, and quickly transform the design results into basic metadata for development.
+	Related skills：The technologies used in the project include ScheduleX which synchronizing project data, ActiveMQ which synchronizing of IAM system permission data, Redis which caching department and project data, and Elasticsearch which searching operation log.
+	Responsibility description：
+1. responsible for the development of core code and optimization of technical solutions.
+2. responsible for development team management,team technical ability improvement,module design guidance,code quality management
+3. In charge of tackling complex d problems, as well as the detection and location of production problems.
+	App Build
+	Project description：The project is a abc project. Using hw HAE framework, the project aims to build a platform for developers. It has a one-stop platform from environment building, project construction to development, deployment and automated testing. Specifically, it includes automatically generating useable front-end engineering (vue) templates, back-end service engineering templates (java), getting through the database, configuring the center, deploying servers, and solving the pain points of developers in building new environment projects. One-stop access to the whole process, allowing developers to focus on business function development.
+	Responsibility description： 
+1. Agile development management, organize the team's daily morning meeting, monitor the team's development tasks
+2. Participate in the analysis, design, development and optimization of core modules
+3. Cooperate with team developers to work on code review, and provide performance optimization and security suggestions
+4. In charge of locating and solving technical problems in the project, providing technical solutions and customized development.
+	Design Desktop
+	Project description：hw's internal systems are numerous and confusion. It needs a platform to manage the systems needed for current work, searching and applying to join the systems of interest. Therefore, desktop design was mandatory. The desktop is designed as the entrance to interface with other internal systems of Huawei and display other systems. Its main functions include personal space, project registration, project management, user permission management, project monitoring, project display, etc.
+	Related skills：It’s used  spring cloud as architecture,Eureka as the registry, Feign as service registration and service invocation, Zuul as the gateway, Config as the configuration center, Kafka as the message bus, Mybatis as operating the database and Oracle as the database. The fore-end technology is Angular JS.
+	Responsibility description：
+1. Be responsible for product technical architecture design, analysis,scheme selection and formulation
+2. responsible for core module design and development of core code
+3. Review the system design and code quality,improving the reuse of common technologies
+4. Be responsible for system performance optimization, quality improvement, and tackling key technical problems
+5. Participate in the development of development specifications, technical standards, preparation of the corresponding technology
+
+
+Good morning!  Thank you for taking your time.It is really my honor to have this opportunity for an interview, and I hope I can make a good performance today.Now, I would like to introduce myself briefly.
+my name is xx and I come from hn province, and I have graduated from hn Institute of Technology.from then on,have accumulated 12 year’s experience in software development and nearly 6 year’s experience in team management, rich experience in distributed system architecture, good at complex business analysis and design, solving difficult problems, team building,project risk control and strong learning ability.I like programming,I've done a lot of projects and be able to use many developed tools.I have mastered many programming technology.for example Spring ,spring cloud and relevant tools,mybatis,redis,MQ middleware and so on.moreover,i'm Familiar with DevOps tools and project management tools like jira,confluence,and project development method like agile.
+that' all,thank you for your attention.
+
+my fail project expericess
+my fail-project not exists in resume.
+that project is to revamp front page to adapt for vue.
+because of vue is good at various type of screen.like computer,mobile and tablet.
+firstly ,we think we just instead of all view page and a litte back-end code. As leader told us that it's just for front-end.but actually,it cost many time in revamping back-end interface.
+we spend several weeked to complete it.from then on,i learnd we need to know whole system.
+
+
+Agile is a methodology and behavioral framework that aims to encourage timely production and rapidly deliver software products with high quality.Agile development is a human centered and iterative development method,now the popular agile method include scrum,extreme programming,test-driven development,kanban,lean and so on.usually i use scrum to function my project.
+the scrum have some ceremonies for example sprint,sprint plan,daily stand-up,sprint review,sprint retrospective.it alse need some role to support.
+Product Owner role represents the Business side and is responsible for the maintaining the list of product features wish list called a product backlog and sets priorities for development,alse need to clarify the Product Backlog and Ensure the Product Backlog is visible, transparent, and clear to all.
+The Scrum Master role is responsible for helping the team  to build the product according to the Scrum framework,and ensureing the Scrum Team adheres to Scrum theory, practices, and rules,even protecting team from any interference that might prevent them from completing their sprint goals.
+the Development Team organizes themselves to determine the highest priority product backlog items they will work on during the sprint and decides how to implement those pieces.this usually happend during sprint planning.
+we every moning hold daily stand-up meeting. in this meeting,all team member answer three question,as follow:what did you do yesteday,what do you plan to do next,and any potential blocks.
+we hold Sprint Review meeting at the end of the Sprint to inspect the Increment and adapt the Product Backlog if needed.
+we alse hold Sprint Retrospective meeting,this meeting provide an opportunity for the Scrum Team to inspect itself and create a plan for improvements to be enacted during the next Sprint.
+
+敏捷和传统项目管理（瀑布）之间有什么区别？
+Agile encourages design,development,testing and almost all event to be performed at the same time. Instead, the traditional project method often closes and completes a phase before the next start. Agile encourages short, frequent feedback. In a waterfall, feedback is usually not collected until the end of the project.The typical problems of waterfall model are long cycle and difficult change. Agile development means rapid iteration, continuous integration and embracing change.
+we maybe consider waterfall model,If the requirement is simple, predictable, fully defined and understood, and espeicially will not change.
+
+
+
+
+当时使用了微服务、CICD、容器化、自动化,agile development.实施敏捷开发，前期要做好很好的设计功底，该拆的拆该细化的细化。微服务按业务拆分，做到业务层面的高内聚低耦合。微服务方便扩展。
+作为项目经理，我们注重设计稳定基础架构,我主要负责或者带领核心开发人员构建良好的基础框架并编写封装公共代码,
+当时推动的时候，不能一蹴而就，要使用新的业务模块来演进。将风险较低的业务走这种路线，打造一个完整的体系后，接下来就是其他业务逐步迁移的事了。实施过程中，先从自己负责的范围内推进和落实，经过1年左右横向推动到其他团队甚至整个公司。过程中最重要的是需要公司领导和重要技术岗位成员的支持，项目中推动的时候有一些成员排斥、遇到了一些阻力,跟相关人说明这样做的原因，这是公司领导层对未来趋势的要求。产生的效果很明显，老板经常能看到产品在变动，不断有功能上线也获得了用户的反馈。
+this project characteristic is agile development,microservices, CICD, containerization,automation.we implement project with agile development theory.In the early stage a good design foundation should be made,Functions are split and refined as needed.entire system are decomposed into Microservices by business to achieve high cohesion and low coupling at the business level. Microservices are alse good for expansion.we pay more attention to the design to make the solid infrastructure,I in charge of manageing and leading the core developers to build a good basic framework,i alse code and encapsulate public function。
+At that time, we use new business modules to evolve.Cause taking this route to build a complete system for businesses with low risk.after that we migrate gradually other businesses. During the implementation process,at first we promote and implement from the scope of our own responsibility, and then promote to other teams or even the whole company after about a year.it's important to get the support of the company's leaders and members in key technical posts. Some members repel and encounter some resistance when pushing. we need to explain the reason for this to the relevant people, and emphasize that this is the requirement of the company's leadership for the future trend. The effect is very obvious. The boss can often see new feature, and we obtain many user's feedback when the product is continuously functional online.
+CI是基于Jenkins，基于git管理代码，因为git更适用于CI，分支和版本很强大。开发者提交代码后，Jenkins可以感知代码变动，会自动对代码进行编译，如果编译失败发送邮件通知开发者。同时会自动触发sonarqube进行代码扫描，开发者可以到sonar平台查看代码的质量问题，对存在的隐患bug或不规范写法进行纠正，很多时候机器比人靠谱所以代码扫描很有必要，重要的业务逻辑需要人工参与代码review。使用pipeline脚本做了配置，针对develop、release、master分支会自动将编译后的包打成docker镜像并自动发布到docker仓库。重要的业务必须要写junit用例，虽然junit用例会增加开发量，根据项目和公司的质量情况做的要求是针对核心和重要业务必须写，之所以没有强制全部写因为需要代价，更要花啊，代价和成本是考量项目成功的重要因素。
+our CI is based on Jenkins.code management is based on git. Because git is more applicable to CI, branches and versions are very powerful. After the developer submits the code, Jenkins can sense the code changes and automatically compile the code. If the compilation fails, he sends an email to notify the developer. At the same time, Sonarqube will be automatically triggered for code scanning. Developers can check the quality of the code on the Sonar platform and correct the hidden dangers, bugs or non-standard writing methods. In many cases, machines are more reliable than people, so code scanning is necessary. Important business logic requires manual participation in code review. The pipeline script is used for configuration. For the development, release, and master branches, the compiled package will be automatically marked as a docker image and automatically published to the docker warehouse. Junit use cases must be written for important businesses. Although junit use cases will increase the amount of development, it is required to write for core and important businesses according to the quality of the project and the company. The reason why it is not mandatory to write all of them is because it requires costs, which are important factors to consider the success of the project.
+CD基于Jenkins触发服务器脚本来实现自动发布，服务器脚本前期采用docker-compose简单化编排，在直接Jenkins操作服务器脚本完成自动部署。
+The CD is automatically released based on Jenkins triggering the server script. The server script is arranged simply by docker compose in the early stage, and is automatically deployed by Jenkins operating the server script directly.
+使用微服务进行敏捷开发要解决哪些问题？
+服务被颗粒化之后，就更适合容器化了，现在docker+springboot微服务组合比较常见。
+After the service is granulated, it is more suitable for containerization. Docker+spring boot microservice composition is common now.
+服务多了，就要管理，管理代价就会提示，加上容器化之后容器可能随时升级被整体删除替代。加上springboot的打包方式（jar）和docker镜像的方式，使得配置可变性变的困难。文件存储、日志、配置、调度，这些都要提取进行统一管理，服务的注册发现使用Spring的Eureka，服务调用使用Feign。部署使用docker容器。
+When there are many services, management is required, and the management cost will be prompted. After containerization, the container may be upgraded at any time and replaced by the overall deletion. In addition to the spring boot packaging method (jar) and Docker image, it is difficult to configure variability. File storage, log, configuration and scheduling should be extracted for unified management. Spring Eureka is used for service registration and discovery, and Feign is used for service invocation. Docker container is used for deployment.
+配置中心你使用过Spring Cloud Config、Nacos、Disconf，现在使用Apollo。
+调度中心，用xxljob。
+服务化之后，服务变多了。全链路跟踪也变的更为必要，这个你之前的方案是SpringCloud Sleuth+Zipkin。监控方面，SpringCloud的Hystrix Dashboard+ Turbine.
+After service, there are more services. Full link tracking has become more necessary. Your previous solution is SpringCloud Sleuth+Zipkin. In terms of monitoring, Spring Cloud's Hystrix Dashboard+Turbine
 }
