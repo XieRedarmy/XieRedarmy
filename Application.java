d0140c51bac2b72bddd8184344c881dd76623de7
XieRedarmy <56301584@qq.com>
2022-10-24 13:49:20 +0800

    Update Application.java

--- Application.java
+++ Application.java
@@ -1,3 +1,124 @@
 public class Applicaton{
-  
+  A disruptive, out of the box leader serving as an accomplished and results-oriented project/program management professional with exceptional negotiating and analytical skills. A leader and subject matter expert with emphasis on program management, strategic execution, and customer/ stakeholder relationship development.  Astute in identifying areas in need of improvement with the vision to develop, implement and execute successful action plans cross-functionally regarding sales, operations, services and vendor organizations for optimized performance and maximum financial return. Keenly knowledgeable in various technologies with sound understanding of how to serve the company’s overall strategy and competitive edge through leadership, collaboration, and execution. 
+
+Certification
+PMI Certified PMP (No: 0000001, Grant Date: 22 Sep 2012).
+Professional Attributes: 13 years’ IT experiences (as Agile Coach, Software Architect, Business Analyst, Team Leader, Scrum Master and PM), 7 years’ PM experiences, agile, adaptable, proactive, persuasive, assertive, collaborative, perceptive, methodical, and resourceful with keen attention to detail. End-to-End project delivery experiences (from Pre-sales to Day 2 support). 
+
+Agile/ Innovative thinking/Hybrid● Problem Analysis and Resolution ● Customer Success/ Stakeholder Mapping  ●  Budgeting (Expenses and Forecasting) ● Program/Project Management   ●Vendor Management ● Communications & Reporting ●Team Leadership & Collaboration  ● Risk Management ● Customer Relations ● Promotional Activities ● Mandarin and English Fluent; Japanese Familiar.
+
+Technological Proficiencies
+•	Network, Data Centre, Security, Java, C#.Net, Python, Big Data, Web Service, Oracle, BD2, be familiar with OOA, OOD, Big Data, and Cloud Computing etc.
+Profile of Key Strengths
+
+•	Ability to lead and manage multiple (concurrent) projects/ products, often revenue impacting, requiring collaboration among cross-functional resources often across a wide geographic area with strong ability to bridge various support groups to ensure all tasks are completed
+•	Primary Verticals worked within, Financial Services, Insurance, Banking, Government, College, Manufacturing, and High-Tech.
+•	Noted for Outstanding Thought Leadership, Coaching and Customer Service; Recognized by management, partners, colleagues, and peers as a key contributor and highly valued member of the team who balances project objectives while ensuring team engagement and enlisting stakeholders’ concurrence 
+
+•	A strong team leader with clear ideas on how to manage and optimize projects to achieve company goals and serve customer needs; Organizes projects and mobilizes teams while simultaneously, maintaining structure and direction.  Promotes insight and common understanding among team members via superior verbal and written communication skills and by resolving ambiguity;  Seamlessly anticipate, prioritize, and delegate work activities and leverage indirect resources to meet objectives
+
+•	Proficient in delivering C-Level Executive Presentations/ Briefings; Expert ability to communicate, counsel and advise clearly and effectively, both orally and in writing with diplomacy and tact with all levels of staff and individuals. Natural ability to clearly articulate complex and abstract ideas 
+
+•	Leverages business knowledge and market trends to deliver recommendations; High-level ability to recommend improvement and standardization; Sees how problems can be solved and enlists  support in executing new guidelines for consistently successful product creation and  implementation 
+
+•	Excels in customized service and support, and exhibits exceptional interpersonal and relationship building skill   
+
+Professional Experience
+
+Systems, Inc. 
+Position:   
+Manage various types’ projects like Software, Security, Network and Datacenter PDI etc. cross APJC (Great China-HK & TW, Singapore, Japan, Australia, and New Zealand).
+Manage distributed teams across geographies in delivery& development.
+Work with Account Team to identify Opportunities and develop Proposal for winning the deal.
+Interact with Engineers, Senior Management, Vendor (from PO Negotiation), and End Customer.
+Convene Kick off, Requirement Collection, Workshop, and Knowledge Transfer etc. F2F meetings with Customer. 
+Create and maintain timelines, and keep large, diverse groups informed of progress and obstacles.
+Apply and tailor Agile methodologies in Agile Value Stream Operating model for grogram and project levels.
+Manage Project Delivery (Cost, Revenue, Quality, and Scope etc.), monitor Project Lifecycle, identify and manage risks and issues, adopt PMI’s and Scrum Alliance’s Agile Methodologies, manage Communication, define Success Metrics, build Team, and escalate issues in an appropriate manner etc.
+ Co., Ltd. 
+Position:  Project Manager, Software Architect                                                                                                    2013/05 - 2014/08
+Be in charge of Software Development Project Management, Requirement Analysis, As-Is Business Collection, and Analyst, To-Be Solution Design, Risk Identification and Management, Technology Selection, Technical Issue Investigation, and Solutions.
+Worked with engineers to develop detailed and creative solutions to complex problems.
+Assisted in creating solutions for client and/or internal review.
+Abided by team coding standards, development processes, and best practices.
+ Co., Ltd. 
+Position:  Lead Associate, Software Architect                                                                                                      2011/11 – 2013/04
+As Team Leader (BA&SA), be in charge of Software Development Project Management, Requirement Analysis, analyzing End Customer’s Requirement, designing system, choosing Technology Framework and Language, modeling Business Case, designing Database and etc.
+
+IBM Co., Ltd (Camelot). 
+Position:  IT Specialist, Software Architect                                                                                                            2009/11 – 2011/08
+As EPR L2 Team Leader, be in charge of CRs, reviewing design, monitoring system, problem management, how-to document management, deploying periodic updates, guiding coding and test, and providing solutions to Support (L2) Team and developing CR (L3).
+
+ Co., Ltd. 
+Position:  Software Engineer                                                                                                                                        2008/01 – 2009/10
+As SE, understanding the requirements, coding, bug fixing and maintenance of the product, coordinating with team members.
+
+PROJECT EXPERIENCE
+
+Project Manager-2019/04 – now
+
+Rakuten will be the world's first zero-touch, fully automated and IT-driven “Mobile Telcocloud” build to serve tens of millions of subscribers across the globe. Mobile Telcocloud will be built with zero-touch, software-defined, fully virtualized capabilities using cloud Native technologies. 
+As Project Manager to be onsite to lead this world first cloud automation project with global team and local Japan Engineers.
+
+Project Manager-                                            2018/06 – 2019/03
+
+The scope of this project is to do Network design and implementation to migrate UNSW data from its current in-house data center to an off-premise data center facility.
+As Project Manager, managed this End to End (from presale to go live) project, assist Account Team to develop proposal for winning the deal, defined project deliverables, milestones, sprint plans and release plans, managed priorities and communicate technical impact to stakeholders, communicated both within the team and outside the team, to ensure confidence at all levels.
+
+Project Manager-                                                                       2017/06 – 2018/10
+
+Successfully managed two sophisticated Software-defined networking projects. My responsibilities include working with Engineers to develop detailed and creative solutions to complex problems, status reporting, convening regular meetings, milestone and deliverable tracking in an agile atmosphere. Worked to inject innovation and continuous improvement across the team by working with globally diverse stakeholders in a virtual setting. 
+
+Project Manager- Azure Stack Implementations (Taiwan, Singapore, and Hong Kong)                                                            2018/07 – 2019/02
+
+Azure Stack Design and Implementations. (Tech involved Microsoft Azure). 
+As Project Manager, led Vendor India Engineers and local Network Engineer team to implement this Azure Stack Program in Taiwan. Ensured consistency in deployed configurations and track exceptions for follow up.
+
+Project Manager-                                           2018/07 – 2018/12
+
+Volkswagen Beijing new Datacenter ACI Design, Migration and Support. (Tech involved SDN and ACI). 
+As Project Manager, defined and assessed project risks, both technical and non-technical in nature and effectively deploy mitigation plans, led local Consultants and Partner Engineers to design, did onsite migration and knowledge transfer for Volkswagen new Datacenter. 
+
+Project Manager-       2017/09 – 2018/10
+
+The scope of this project is to Plan, Design & do Implementation for Bank of Dalian new Datacenter.
+As Project Manager, managed this End to End (from presale to go live) project, assist Account Team to develop proposal for winning the deal,  negotiated with sub-contractor,  defined project deliverables, milestones, sprint plans and release plans, managed priorities and communicate technical impact to stakeholders, communicated both within the team and outside the team, to ensure confidence at all levels.
+
+Project Manager-                                                                 2017/09 – 2018/09
+
+Developed detailed DC security standard and guidance for Bank of China. 
+As Project Manager, be in charge of this Datacenter security-consulting project, worked with Hong Kong Consultant, local network Engineers and Partner to deliver Customer a detailed DC security standard and guidance.
+Lead as a consultant to clients by understanding their expectations and requirements and effectively communicating across functional teams.
+
+Project Manager- Conference Management System Development Project (Cisco)                                         2016/03 – 2017/09
+
+Conference management system provides conference rooms reserving, conference-related resources applying, and Cisco's WebEx Integration functions. (Tech involved Java and Oracle). 
+As Project Manager, be in charge of Requirement Collecting, Analyzing, Tech Framework Choosing, Core Modules Developing, and Implementation. 
+
+Senior Enterprise Architect                                                         2013/06 2014/07
+
+Standardized BPO Processes Management Platform, it has Tasks Assignment Module, Planning Module, Resource Management Module, Dashboard & Report Module and etc. (Tech involved: C# .NET and Oracle).
+As SEA, be in charge of designing Enterprise Architecture, Collecting Requirement, Analyzing, Choosing Technology Framework and Language and developing Core Modules.
+
+Lead Associate (Project Manager) -                    2012/11 2013/06
+
+AB InBev recruitment system, which includes internal portal and external website, HR uses this system to publish and manage position etc. External Applicants and Headhunt use it for position application. (Tech involved Java, JavaScript, and Oracle). 
+As TA participated in project management, requirement collection, requirement analysis, domain model building, prototype design, database design, system technology selection, detailed design etc, to this kind of medium-sized project which needs to be quickly built and delivered, so used hybrid methodology with agile development theory and the RUP constructs.
+
+Lead Associate (Project Manager) -                                                                         2012/12 2013/04
+
+CRM system of Pfizer, the main users are from Sales Dep and Marketing Dep, this system has 7 modules, e.g. Contact Management, Opportunity Management, Engagement Management, Collaboration, Marketing Automation, and Dashboard & Report modules. (Tech involved Java and Oracle). 
+As Project Manager, worked with India Team to analyze End Customer’s Requirement, designed system, designed UI, chose Technology Framework and Language, modeled Business Case, and designed Database etc.
+
+Lead Associate (Project Manager) -                                                      2012/01 2012/11
+CRM system of Genpact internal, the main users are from Sales Dep, this system has 6 modules, e.g. Contact Management, Opportunity Management, Engagement Management, Collaboration, and Dashboard & Report modules. (Tech involved Salesforce.com).
+As Project Manager and Architect, analyzed End Customer’s Requirement, designed system, designed UI, modeled Business Case, and designed Database etc.
+
+IT Specialist -                                                                 2009/11 2011/09
+The Entitled Price Repository (EPR) provides a single data repository of IBM Customer entitled product price information (including Customer Accounts, HVEC, and Xccelerator Product Pricing, Customer Discounts and associated Terms and Conditions). It also provides a method for users to view the repository data through reporting functions and provide export feeds of the repository data to users and downstream systems that need to use this information. (JAVA)
+Event Management: detected events, made sense of them and determined the appropriate control action.
+Incident and Problem Management: restored normal service operation as quickly as possible and minimized the adverse impact on business operations.
+Request Fulfillment: provided a channel for users to request and receive standard services for which a pre-defined approval and qualification process exists, provided information to users and customers about the availability of services and the procedure for obtaining them, sourced and delivered the components of requested standard services, assisted with general information, complaints or comments.
+Problem Management: prevented problems and resulting incidents from happening, to eliminate recurring incidents and to minimize the impact of incidents that cannot be prevented.
+Access Management: provided the right for users to be able to use a service or group of services.
 }
