1344dcc45f89320f3f32b9dcc9a3d1e5abe24f57
XieRedarmy <56301584@qq.com>
2022-10-24 13:59:14 +0800

    Create Start.java

---
+++ Start.java
@@ -0,0 +1,267 @@
+public class Start{
+  Introduce yourself
+● Nearly 13 years of Java development experience, including more than 6 years of practical experience in
+distributed system architecture, and more than 5 years of team management experience, with distributed,
+high concurrency, high availability, large data volume system architecture design and R & D experience, is
+currently responsible for 20 million registered users, more than 3 million daily active users. Architecture
+design and R & D of 100,000 concurrent member marketing platform during peak period.At the same time,
+he has a solid technical foundation, has in-depth research on Zookeeper, Netty, Dubbo, SpringCloud,
+Spring and other open source framework source code, and has some experience in framework
+customization development.
+Skills and expertise
+Deeply understand the underlying principles of JVM, be familiar with the use of various garbage collectors
+of JVM and the tuning of core parameters, have some experience in online problem handling, and have
+their own unique views on JVM tuning.
+● Familiar with the design and performance tuning of common relational data such as Oracle and MySQL
+● Deeply understand the design principles and underlying architecture of Spring, Springmvc, Mybatis and
+other mainstream open source frameworks, read the core source code, and expand as needed
+● Be familiar with the usage scenarios of Redis core data structure, multi-level cache architecture, and
+various high-concurrency cache usage scenarios, such as cache avalanche, cache penetration, cache
+invalidation, and hotspot cache reconstruction.
+● Solve the technical problems of distributed scenarios in the project, such as distributed lock, distributed
+transaction, distributed Session, distributed task, sub-database and sub-table
+● Familiar with Nginx, Tomcat, ElasticSearch, mongodb, Zookeeper and other middleware technologies
+● Familiar with the use of common message middleware Kafka, Rabbitmq and RocketMQ, and have solved
+various problems in message communication scenarios, such as message loss, repeated consumption,
+sequential execution, large-scale backlog and other online problems.
+● Deeply understand the design principles and underlying architecture of open source microservice
+frameworks such as Springboot, Springcloud, Dubbo, Nacos, Feign and Ribbon, study the core source code
+and architecture, and have certain framework customization and development capabilities
+● Have practical experience in domain modeling and middle-platform construction, master common
+architecture design methods and modes, and have the systematic construction capability of highperformance,
+high-availability and scalable systems.
+● Industry e-commerce: B2B2C, (ERP, CRM, SCM)
+Certification
+PMP Project Management Certification
+Work experience
+Co., Ltd. 2021.04-present
+Position: Development Team Leader
+● Be responsible for technical selection, architecture design and module design guidance
+● Be responsible for the development of core code, code Review, technical difficulties, etc.
+● Be responsible for system performance optimization, quality improvement, daily maintenance,
+troubleshooting, etc.
+● Be responsible for development team management, team technical ability improvement, etc.
+Technology Co., Ltd. 2017.03 - 2021.03
+Position: technical manager.
+● Be responsible for product technical architecture design, analysis, requirement review, scheme selection
+and formulation
+● Be responsible for tackling key technical problems, system optimization, code quality management,
+participating in the design and early development of the implementation scheme, and improving the reuse
+of common technologies.
+● Participate in the development of development specifications, technical standards, preparation of the
+corresponding technology, testing, design, requirements and other specification documents, as well as the
+daily development management of the team
+● Undertake the research and introduction of relevant new technologies and trends, and promote internal
+technological progress and innovation
+Technology Co., Ltd. 2015.07 - 2017.03
+Position: technical manager.
+● Cooperate with the company's product department, lead and complete the product delivery
+● Be responsible for system technical scheme evaluation, architecture design, core code development, etc.
+● Be responsible for the implementation of key functions and modules, guide team members, solve
+complex problems encountered in the development process of team members, and share relevant
+technologies ● Cultivate team echelons and improve the overall professional skills of the team 5.
+Compilation of specification document Maintain and assist the project manager to complete relevant work.
+Software System Co., Ltd. 2009.02 - 2015.07
+Position: Project Leader
+1. Serve as the project leader Be responsible for requirement control and design, guide members to
+develop 2. Be responsible for launching the project ? Follow up requirements 3. Serve as project manager
+to communicate with customers and understand the overall requirements of the project. Keep in touch
+with customers, timely feedback the stage results, timely change the reasonable needs of customers, track
+the progress of the project, and coordinate the cooperation among project team members.
+Project experience
+Membership marketing platform 2021.04-present
+Company: (Co., Ltd.)
+Project description:
+Mainly undertake the big ginseng forest group more than 8000 drugstore 20000 more than ten thousand
+member user rights, through the member marketing platform (small program, precision marketing push
+message, preferential activities) for drainage users to offline stores to purchase drugs registered as a
+member, when members buy a certain number of drugs can be upgraded to the supreme member at the
+same time can receive coupons, points, Gift cards,Specific drugs enjoy discounts and other benefits to
+improve the performance and sales turnover of the whole group's stores.
+Application of project technology:
+● Spring cloud microservice architecture system is adopted, Springboot is used to build applications, nacos
+is used for service registration and discovery, openFeign is used for interfaces between services, Hystrix is
+used to realize fuse degradation and error handling, and Getway manages external system access.
+● Task scheduling uses open source xxl-job for fragmented task processing.
+● Use rabbitmq to realize service decoupling, asynchronous processing and peak clipping to improve
+system throughput
+● Application service log collection and search using ELk
+● Use redis to cache hot data, high frequency data, and distributed locks
+Responsibility description:
+● Be responsible for reconstructing the member center: coupons, member login and registration,
+authentication
+● Be responsible for building the infrastructure, developing the basic components, sorting out the original
+business modules and splitting the services according to the DDD domain modeling
+● Be responsible for the overall design, development and online deployment of store service and
+promotion activities (order and payment)
+● Be responsible for full link pressure test and performance tuning of core interface
+Supply chain system 2020.03 - 2021.04
+Project description:
+It mainly undertakes the whole process from delivery to logistics in the follow-up links of the company's ecommerce
+platform B-end and ERP system C-end orders, including procurement management. Inventory
+management, commodity management, financial management, a complete set of process system.It solves
+the communication problems and data transmission timeliness problems between brand operators and
+suppliers caused by scattered suppliers, too many product varieties and too frequent orders, and shortens
+the production cycle. ? Reduce business operating costs.
+Application of project technology:
+● Spring cloud microservice architecture system is adopted, Springboot is used to build applications, nacos
+is used for service registration and discovery, openFeign is used for interfaces between services, Hystrix is
+used to realize fuse degradation and error handling, and Getway manages external system access.
+● Task scheduling uses open source xxl-job for fragmented task processing.
+● Realize service decoupling, asynchronous processing and peak clipping by using Kafka to improve system
+throughput
+● Application service log collection and search using ELk
+● Use Redis to cache hot data, high frequency data, and distributed locks
+Responsibility description:
+● Build a micro-service framework, and split the business platform according to the business: user center,
+warehouse center, scheduling center, inventory center, logistics center, commodity center and other
+general center capability services
+● Participate in the analysis, design, development and tuning of core modules;
+● Cooperate with team developers for Code review work, requirements, code quality control, training and
+guidance of related technologies
+● Be responsible for the technical problems encountered in the project, as well as the troubleshooting,
+positioning and solution of online problems, the formulation of technical solutions, and customized
+development.
+Victor Membership Marketing System 2019.07 - 2020.03
+Company: (Technology Co., Ltd.)
+Application of project technology:
+● Spring cloud microservice architecture system is adopted, Springboot is used to build applications, nacos
+is used for service registration and discovery, openFeign is used for interfaces between services, Hystrix is
+used to realize fuse degradation and error handling, and Getway manages external system access.
+● Task scheduling uses the open source quartz framework.
+● Use Rocketmq to realize service decoupling, asynchronous processing, peak clipping, and improve
+system throughput
+● Application service log collection and search using ELk
+● Use Redis to cache hot data, high frequency data, and distributed locks
+Project description:
+Beijing Weikeduo Garment Manufacturing Center is a modern garment enterprise integrating advanced
+garment design, research and development, production and sales. At present, the membership
+management system of the whole company is relatively backward. It conducts research on membership
+and marketing, and builds a new membership and marketing management system based on the ideas and
+rules of online and offline integrated operation.Carry out unified management of all channels for
+members, and realize the grouping and labeling of member data. So as to provide more accurate and
+detailed latitude for member operators to operate. Support online and offline integrated membership
+marketing. Track and analyze marketing effectiveness through big data capabilities to support operators.
+Responsibility description:
+● Agile development management, organize the team's daily morning meeting, formulate and assign the
+team's development tasks.
+● Participate in the analysis, design, development and tuning of core modules;
+● Cooperate with team developers for Code review, and provide performance optimization and security
+suggestions
+● Be responsible for positioning and solving technical problems encountered in the project, formulating
+technical solutions and customized development.
+Perfect Digital Mall Project 2018.08 - 2019.07
+Company: (Co., Ltd.)
+Application of project technology:
+● Spring cloud microservice architecture system is adopted, Springboot is used to build applications, nacos
+is used for service registration and discovery, openFeign is used for interfaces between services, Hystrix is
+used to realize fuse degradation and error handling, and Getway manages external system access.
+● Task scheduling uses the open source quartz framework.
+● Use Rocketmq to realize service decoupling, asynchronous processing, peak clipping, and improve
+system throughput
+Elastics earch is used to search for commodities, and cannel is used to subscribe to MySQL binlog to
+realize real-time commodity data.
+● Use Redis to cache hot data, high frequency data, and distributed locks
+Project description:
+Perfect (China) is a company that sells health food, small kitchenware, cosmetics, cleaning products and
+personal care products. The purpose of building a digital mall is to solve online sales, improve direct sales
+personnel, expand business volume and maximize enterprise value. The mall mainly includes three parts:
+mall, exhibition tools and store management functions.To provide consumers with multi-scene, multicontact
+diversified applications, online and offline goods, members, services interconnection, to provide
+consumers with the ultimate experience. Data-driven consumer experience, sharing economy, social sales,
+find the perfect new business growth point
+Responsibility description:
+● Be responsible for business requirement control, function development and testing;
+● Lead the architecture design and core module design of the project;
+● Review the system design and code quality of development engineers, and prepare technical
+specifications and development specifications;
+● Take the lead to reconstruct the system, optimize the system, ensure high performance processing and
+system stability, launch the project and solve the problems encountered in the production environment;
+● Responsible for training and coaching junior development engineers
+abc 020 Mall 2015.07 - 2018.08
+Company: (Network Technology Co., Ltd.)
+Project description:
+abc Mall is a self-operated online shopping mall based on fresh food. At present, it is mainly green
+and pollution-free vegetables. Firewood eggs, mainly black pork products. With "Green, safe, fast, good
+service for the purpose" ", currently only Shijiazhuang as a starting point for sales, will be extended to the
+country. The vegetables are purchased to the supply center. The supply center weighs the raw vegetables,
+puts them in storage, and removes the loss.Into refined vegetables into storage, and then sent to various
+stores, stores store refined vegetables, offline sales mainly to card users. You can get a card offline, but if
+you want to pay for vegetables online, you have to bind online registered users.
+Application of project technology:
+● Service registration and discovery and service governance based on dubbo
+● Task scheduling uses the open source quartz framework.
+● Use rabbitmq to realize service decoupling, asynchronous processing and peak clipping to improve
+system throughput
+● Product search with elastics earch
+● Use redis to cache hot data, high frequency data, and distributed locks
+Responsibility description:
+● Responsible for the development of commodity management, trading and order management
+● Be responsible for tackling key technical problems, system optimization and production troubleshooting.
+● Be responsible for solving technical problems in the development process and helping team members to
+grow rapidly
+● Lead the project launch and operation and maintenance work
+abc Supply Chain Financing System 2014.10 - 2015.07
+Company: (Software System Co., Ltd.)
+Project description:
+Supply chain financial services refer to taking the relevant enterprises in the supply chain as a whole,
+setting up financing schemes according to the chain relationship, industry characteristics and assets such
+as inventory, advance payment and accounts receivable in commodity transactions, injecting funds into
+relatively weak small and medium-sized enterprises, and providing financial services for large
+enterprises.So as to solve the problem of unbalanced distribution of funds in the supply chain and
+enhance the group competitiveness of the entire supply chain enterprises.
+Project technology application: Spring, Spring MVC, MyBatis, MySql, Git, Maven, Redis, JSON, quartz,
+multithreading
+Responsibility description:
+● Formulate project development plan documents, quantify tasks, assign them to relevant personnel
+reasonably, track project progress, and coordinate cooperation among project team members
+● Be responsible for building, upgrading and reconstructing the project framework
+● Be responsible for financial statement development and project requirement analysis
+Payment Platform Project of Guangzhou Rural Commercial Bank 2012.12 - 2014.10
+Project description:
+The payment platform of Guangzhou Rural Commercial Bank is a platform for banks to accept payment
+instructions submitted by customers through PC, mobile phone and other terminals, and quickly realize
+fund clearing with third-party payment companies, electronic shopping malls, bank card institutions,
+interbank institutions, etc. It is a sharp weapon for banks to expand electronic payment and settlement
+business.At present, the payment platform of most banks is changing from the original payment platform
+to the guarantee intermediary platform.
+Project technology application: Spring, Spring MVC, MyBatis, MySql, Git, Maven, Redis, JSON, quartz,
+multithreading Responsibility description:
+● Participate in project requirement analysis, scheme discussion, technical research, and write relevant
+technical documents
+● Be responsible for the functional transformation and design of the new payment platform 2.0
+● Be responsible for the development of payment, refund and other core modules
+CGB Financial IC Card Multi-application Platform Project 2010.12 - 2014.12
+Company: (Software System Co., Ltd.)
+Project description:
+Ningbo Pedestrian Bank has taken the lead in developing Ningbo Financial IC Card Multi-Application Public
+Service Platform, which is responsible for the unified management of industry applications, the routing
+management of industry application downloads, and the handling fees related to industry app lications, in
+combination with the industry applications based on financial IC cards developed by various commercial
+banks and industry application units in Ningbo.Finally, the goal of "one card for multiple purposes and
+application sharing" is achieved.
+Project technology application: Spring, Spring MVC, MyBatis, MySql, Git, Maven, JSON, quartz,
+multithreading
+Responsibility description:
+● Be responsible for the establishment of system framework
+● Interface design and development
+● System functional design, coding implementation and internal testing
+● Be responsible for controlling requirements, communicating with customers and coordinating work.
+Liaotong Card Merchant Value-added Management System 2009.02 - 2012.12
+Company: (Shanghai Huateng Software System Co., Ltd.)
+Project description:
+CGB cooperates with abc Co., Ltd. (Hereinafter referred
+to as "abc") to issue co-branded credit cards for vehicle owners who have the demand for
+expressway in Liaoning Province. The co-branded card is established on the existing vehicle owner card
+platform of CGB, and its basic functions are the same as those of the existing vehicle owner card, and it
+can also realize the toll withholding service and the life cycle management of Liaotong Card.Guangfa
+Liaotong Co-branded Card is not only a credit card of Guangfa, but also a charge card of Liaoning
+expressway. The cardholder can use the financial function of Guangfa Credit Card and the payment
+function of expressway.
+Project technology application: Spring, Spring MVC, MyBatis, MySql, Git, Maven, JSON, quartz,
+multithreading
+Responsibility description:
+● System functional design, coding implementation and internal testing
+● Be responsible for putting the project into production, tracking the later maintenance work, and
+developing new requirements.
+}
